33606ffa53faaf53009b3966e7339d2998bf2554
Arthur Assis <arthur120496@gmail.com>
2019-05-15 19:30:50 -0300

    creates _trace_routes methods

--- router/map_route/map_route.py
+++ router/map_route/map_route.py
@@ -8,16 +8,16 @@
 # |                            |
 # (xb,yb)----------------------(xb,ye)
 
-
 class MapRouter():
     def __init__(self, x_begin, x_end, y_begin, y_end):
-        points = [
+        self.points = [
             (x_begin, y_begin), (x_begin, y_end),
             (x_end, y_begin), (x_end, y_end)]
-        area_of_operations = []
-        on_route = False
-        evasion_in_progress = False
-        current_position = (0, 0)   # tuple(latitude, longitude)
+        self.area_of_operations = []
+        self.on_route = False
+        self.evasion_in_progress = False
+        self.current_position = (5, 5)   # tuple(latitude, longitude)
+        self.base_location = (0, 0)   # tuple(latitude, longitude)
 
     def set_area(self):
         """
@@ -38,11 +38,72 @@ class MapRouter():
         # stuff
         self.evasion_in_progress = False
 
-    def _trace_route(self, parameter_list):
+    # To trace the routes it is assumed that the points are in minutes if
+    # they are not in seconds they must be converted before this method.
+    def _trace_collection_route(self):
         """
         Traces a route using the area of operations map.
         """
-        pass
+        if(self.current_position[0] < self.points[0][0] or self.current_position[0] > self.points[3][0] or self.current_position[1] < self.points[0][1] or self.current_position[1] > self.points[3][1]):
+            return[]
+        route = []
+
+        #five meters in graus
+        five_meters = 0.00004499633
+
+        x = self.current_position[0]
+        y = self.current_position[1]
+        route.append((x,y))
+
+        
+        # goes to the point x0 
+        while(x - self.points[0][0] >= five_meters):
+            x -= five_meters 
+            route.append((x,y))
+
+        # goes to the point y0 
+        while(y - self.points[0][1] >= five_meters):
+            y -= five_meters 
+            route.append((x,y))
+
+        # travels the whole area        
+        while(self.points[3][0] - x >= five_meters):
+            while(self.points[3][1] - y >= five_meters):
+                y += five_meters 
+                route.append((x,y))
+
+            x += five_meters 
+            route.append((x,y))
+
+        return route
+
+    def _trace_base_route(self):
+
+        five_meters = 0.00004499633
+        
+        x = self.current_position[0]
+        y = self.current_position[1]
+
+        route = []
+
+        if(x > self.base_location[0]):
+            while(x - self.base_location[0] >= five_meters):
+                x -= five_meters
+                route.append((x,y))
+        else:
+            while(self.base_location[0] - x  >= five_meters):
+                x += five_meters
+                route.append((x,y))
+
+        if(y > self.base_location[1]):
+            while(y - self.base_location[1] >= five_meters):
+                y -= five_meters
+                route.append((x,y))
+        else:
+            while(self.base_location[1] - y  >= five_meters):
+                y += five_meters
+                route.append((x,y))
+        return route
 
     def _trace_evasion(self, parameter_list):
         """
