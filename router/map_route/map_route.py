484c74e9f299c2e33859cdd3386a2a60e9d02a75
Andre Filho <andre.filho001@outlook.com>
2019-05-31 16:29:04 -0300

    Adds a evasion route stuff

--- router/map_route/map_route.py
+++ router/map_route/map_route.py
@@ -27,9 +27,9 @@ class MapRouter():
     """
 
     def __init__(self, begin, end, current, base):
-        self.points = [begin, end]  # begin and end are tuples of floats
-        self.current_position = current   # tuple(latitude, longitude)
-        self.base_location = base   # tuple(latitude, longitude)
+        self.points = [begin, end]
+        self.current_position = current
+        self.base_location = base
 
     def trace_diagonal_route(self, destination):
         """
@@ -56,13 +56,16 @@ class MapRouter():
         return route
 
     def trace_route_to_base(self):
+        """
+        Traces a route back to the base.
+        """
         return self.trace_diagonal_route(self.base_location)
 
     def trace_collection_route(self):
         """
         Traces a route using the area of operations map.
         To trace the routes it is assumed that the points are in minutes if
-        they are not in seconds they must be converted before this method is
+        they are not in minutes conversion must happen before this method is
         used.
         """
         route = []
@@ -84,8 +87,44 @@ class MapRouter():
 
         return route
 
-    def trace_evasion_route(self, parameter_list):
+    def trace_evasion_route(self, route, blocked_pos, direction=None):
         """
-        Traces an evasion route for the evade function
+        Traces an evasion route for the evade function. Must receive the blocked
+        geolocation param.
+        Returns a new route with the evasion manuver.
+
+        Args:
+        route = []
+
+        blocked_pos = (lat, long)
+
+        direction = None
         """
-        pass
+        reusable_route = route[blocked_pos:]
+
+        if direction is not None:
+            middle = self._get_center()
+            return trace_evasion_route(route, blocked_pos, direction=direction)
+
+        adjacent_blocked_route = tuple(
+            blocked_pos[0], blocked_pos[1] + GPS_PRECISION)
+
+        route_to_adjacent = self.trace_diagonal_route(adjacent_blocked_route)
+
+        if blocked_pos == route[-1]:
+            return route_to_adjacent
+
+        return route_to_adjacent + reusable_route
+
+    def set_current_pos(self, geolocation):
+        """
+        Setter for the current_pos attr.
+        """
+        self.current_position = geolocation
+
+    def _get_center(self):
+        """
+        Gets the middle point inside the operation area.
+        """
+        return tuple((self.points[0][0] - self.points[1][0]) / 2,
+                     (self.points[0][1] - self.points[1][1]) / 2)
